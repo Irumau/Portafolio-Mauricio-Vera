00c87fd066b9519d84b675a9ff842a447c3f46e7
Mauri <mauriciovera272@gmail.com>
2022-11-27 20:05:34 -0300

    Se ha realizado y subido portafolio con layout responsive

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const section = document.querySelectorAll('.btn-link')
+const reBack = document.querySelector('#reback')
+const tl = gsap.timeline({repeat: 0, yoyo: true});
+
+
+const hamburgerButton = document.getElementById('menuHamburger')
+const mainMenu = document.getElementById('mainMenu')
+
+tl.from('#imgIm',{
+    start: true,
+    duration: 2,
+    x: '50vw',
+    ease: 'bounce',
+})
+
+section.forEach((element,index)=>{
+    element.addEventListener('click',()=>{
+        gsap.to(window, {duration: 1, scrollTo:{y:'#section' + (index + 1), offsetY:75}})
+    })
+})
+
+reBack.addEventListener('click',()=>{
+    gsap.to(window, {duration:0.75,scrollTo:{y:'#reback', offsetY:0}})
+})
+
+
+hamburgerButton.addEventListener('click',()=>{
+    mainMenu.classList.toggle('nav-show')
+    tl.from('#mainMenu',{
+        duration: 0.5,
+        x:'20vw',
+        ease: 'ease'
+    })
+})
