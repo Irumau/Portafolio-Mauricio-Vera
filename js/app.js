24c774ace960382833fb3df713dc4ef9c0f99276
Mauri <mauriciovera272@gmail.com>
2022-11-27 21:43:08 -0300

    Probando si se soluciono el espaciado generado por AOS

--- js/app.js
+++ js/app.js
@@ -1,34 +1,41 @@
-const section = document.querySelectorAll('.btn-link')
-const reBack = document.querySelector('#reback')
-const tl = gsap.timeline({repeat: 0, yoyo: true});
+const section = document.querySelectorAll(".btn-link");
+const reBack = document.querySelector("#reback");
+const tl = gsap.timeline({ repeat: 0, yoyo: true });
+const hamburgerButton = document.getElementById("menuHamburger");
+const mainMenu = document.getElementById("mainMenu");
 
-
-const hamburgerButton = document.getElementById('menuHamburger')
-const mainMenu = document.getElementById('mainMenu')
-
-tl.from('#imgIm',{
+tl.from("#imgIm", {
     start: true,
     duration: 2,
-    x: '50vw',
-    ease: 'bounce',
-})
+    x: "50vw",
+    ease: "bounce",
+});
 
-section.forEach((element,index)=>{
-    element.addEventListener('click',()=>{
-        gsap.to(window, {duration: 1, scrollTo:{y:'#section' + (index + 1), offsetY:75}})
-    })
-})
+section.forEach((element, index) => {
+    element.addEventListener("click", () => {
+        gsap.to(window, {
+            duration: 1,
+            scrollTo: { y: "#section" + (index + 1), offsetY: 75 },
+        });
+    });
+});
 
-reBack.addEventListener('click',()=>{
-    gsap.to(window, {duration:0.75,scrollTo:{y:'#reback', offsetY:0}})
-})
+reBack.addEventListener("click", () => {
+    gsap.to(window, { duration: 0.75, scrollTo: { y: "#reback", offsetY: 0 } });
+});
 
-
-hamburgerButton.addEventListener('click',()=>{
-    mainMenu.classList.toggle('nav-show')
-    tl.from('#mainMenu',{
+hamburgerButton.addEventListener("click", () => {
+    mainMenu.classList.toggle("nav-show");
+    tl.from("#mainMenu", {
         duration: 0.5,
-        x:'20vw',
-        ease: 'ease'
+        x: "20vw",
+        ease: "ease",
+    });
+});
+
+
+useEffect(()=>{
+    AOS.init({
+        disable: window.innerWidth < 768
     })
 })
